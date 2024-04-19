f2d92ae4af62360fa135b423e8a691656fd17740
Alejandro Peña <ja.penat1@uniandes.edu.co>
2024-04-18 19:23:10 -0500

    Add unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BookListComponent } from './book/book-list/book-list.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule, HttpClientModule],
+      declarations: [AppComponent, BookListComponent],
     }).compileComponents();
   });
 
@@ -24,12 +22,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('mynewapp');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, mynewapp');
   });
 });
--- src/app/book/book.service.spec.ts
+++ src/app/book/book.service.spec.ts
@@ -2,11 +2,13 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { BookService } from './book.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('Service: Book', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [BookService]
+      imports: [HttpClientTestingModule],
+      providers: [BookService],
     });
   });
 
